563965c57dfdac70d5c89ca450ea2ea12f535434
abigailwright89 <102739142+abigailwright89@users.noreply.github.com>
2024-01-19 19:09:24 +0000

    errors

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AppNavbar from './components/navbar';
-import Homepage from './components/1.HomePage/homePage';
-import Aboutpage from './components/2.AboutPage/about';
-import Portfoliopage from './components/3.ProjectsPage/portfolio';
-import Contactpage from './components/4.ContactPage/contact';
+import Homepage from './components/HomePage/homePage';
+import Aboutpage from './components/AboutPage/about';
+import Portfoliopage from './components/ProjectsPage/portfolio';
+import Contactpage from './components/ContactPage/contact';
 
 
 
